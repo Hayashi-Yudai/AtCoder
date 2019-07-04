20d978a8e74061f713dab31727e8aabbd8471d88
Yudai Hayashi <yudai0913haya@gmail.com>
2019-07-04 23:23:02 +0900

    Add some problems

--- AtCoder/ABC/ABC132/D.cpp
+++ AtCoder/ABC/ABC132/D.cpp
@@ -2,44 +2,35 @@
 using namespace std;
 typedef long long ll;
 
-ll dp[2005];
-int n, k;
+int INF = 1000000007;
 
-ll split(int i, int m, int k)
+ll dp[2005][2005];
+void compute()
 {
-  ll res = 0;
-  if (i == 1)
+  for (int i = 0; i < 2005; i++)
   {
-    if (k >= m)
+    for (int j = 0; j <= i; j++)
     {
-      return 1;
-    }
-    else
-    {
-      return 0;
+      if (j == 0 || i == j)
+      {
+        dp[i][j] = 1;
+      }
+      else
+      {
+        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % INF;
+      }
     }
   }
-  for (int j = m; j < k; j++)
-  {
-    res += split(i - 1, j, k - j);
-    res = res % 1000000007;
-  }
-
-  return res;
 }
 
 int main()
 {
+  int n, k;
   cin >> n >> k;
-  for (int i = 0; i < 2005; i++)
-  {
-    dp[i] = 0;
-  }
-  dp[1] = 1;
 
+  compute();
   for (int i = 1; i <= k; i++)
   {
-    ll red = split(i, 1, k);
-    ll blue = split(i + 1, 0, n - k);
+    cout << (dp[n - k + 1][i] * dp[k - 1][i - 1]) % INF << endl;
   }
 }
